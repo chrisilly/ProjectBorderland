7464606f9180753db589d578e767281776db7bce
Allan-Majid <112650724+Allan-Majid@users.noreply.github.com>
2023-04-24 12:26:57 +0200

    added phaseshifting code (not working properly, yet)

--- ProjectBorderland/Assets/Scripts/Character/Player/PhaseShifter.cs
+++ ProjectBorderland/Assets/Scripts/Character/Player/PhaseShifter.cs
@@ -2,17 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhaseShifter : MonoBehaviour
+public class PhaseShifting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public enum Phases { DefaultPhase, BluePhase, RedPhase, GreenPhase, YellowPhase }
 
-    // Update is called once per frame
-    void Update()
+    public Phases currentPhase;
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        string collisionLayer = LayerMask.LayerToName(collision.gameObject.layer);
+
+        switch (currentPhase)
+        {
+            case Phases.DefaultPhase:
+                if (collisionLayer == "DefaultPhaseCrystal")
+                {
+                    currentPhase = Phases.DefaultPhase;
+                }
+                else if (collisionLayer == "RedPhaseCrystal")
+                {
+                    currentPhase = Phases.RedPhase;
+                }
+                else if (collisionLayer == "BluePhaseCrystal")
+                {
+                    currentPhase = Phases.BluePhase;
+                }
+                else if (collisionLayer == "GreenPhaseCrystal")
+                {
+                    currentPhase = Phases.GreenPhase;
+                }
+                else if (collisionLayer == "YellowPhaseCrystal")
+                {
+                    currentPhase = Phases.YellowPhase;
+                }
+                break;
+
+            case Phases.BluePhase:
+                if (collisionLayer == "DefaultPhaseCrystal" || collisionLayer == "BluePhaseCrystal" ||
+                    collisionLayer == "RedPhaseCrystal" || collisionLayer == "GreenPhaseCrystal" ||
+                    collisionLayer == "YellowPhaseCrystal" || collisionLayer == "BlueGround" ||
+                    collisionLayer == "BlueWall" || collisionLayer == "BlueCorner" || collisionLayer == "Wall" ||
+                    collisionLayer == "Ground" || collisionLayer == "Corner")
+                {
+                    currentPhase = Phases.BluePhase;
+                }
+                break;
+
+            case Phases.RedPhase:
+                if (collisionLayer == "DefaultPhaseCrystal" || collisionLayer == "BluePhaseCrystal" ||
+                    collisionLayer == "RedPhaseCrystal" || collisionLayer == "GreenPhaseCrystal" ||
+                    collisionLayer == "YellowPhaseCrystal" || collisionLayer == "RedGround" ||
+                    collisionLayer == "RedWall" || collisionLayer == "RedCorner" || collisionLayer == "Wall" ||
+                    collisionLayer == "Ground" || collisionLayer == "Corner")
+                {
+                    currentPhase = Phases.RedPhase;
+                }
+                break;
+
+            case Phases.GreenPhase:
+                if (collisionLayer == "DefaultPhaseCrystal" || collisionLayer == "BluePhaseCrystal" ||
+                    collisionLayer == "RedPhaseCrystal" || collisionLayer == "GreenPhaseCrystal" ||
+                    collisionLayer == "YellowPhaseCrystal" || collisionLayer == "GreenGround" ||
+                    collisionLayer == "GreenWall" || collisionLayer == "GreenCorner" || collisionLayer == "Wall" ||
+                    collisionLayer == "Ground" || collisionLayer == "Corner")
+                {
+                    currentPhase = Phases.GreenPhase;
+                }
+                break;
+
+            case Phases.YellowPhase:
+                if (collisionLayer == "DefaultPhaseCrystal" || collisionLayer == "BluePhaseCrystal" ||
+                    collisionLayer == "RedPhaseCrystal" || collisionLayer == "GreenPhaseCrystal" ||
+                    collisionLayer == "YellowPhaseCrystal" || collisionLayer == "YellowGround" ||
+                    collisionLayer == "YellowWall" || collisionLayer == "YellowCorner" || collisionLayer == "Wall" ||
+                    collisionLayer == "Ground" || collisionLayer == "Corner")
+                {
+                    currentPhase = Phases.YellowPhase;
+                }
+                break;
+        }
     }
 }
