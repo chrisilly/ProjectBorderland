bacdb2e5e97699c9d199b256754acb87a94d763c
Dewiie0 <111650051+Dewiie0@users.noreply.github.com>
2023-04-24 11:40:12 +0200

    added scripts
    adding empty scripts (not completly empty) for the dialogue system

--- ProjectBorderland/Assets/Scripts/Dialogue/DialogueManager.cs
+++ ProjectBorderland/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,17 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ink.Runtime;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject _dialoguePanel;
+    [SerializeField] private TextMeshProUGUI _dialogueText;
+    [SerializeField] private GameObject _continueButton;
+
+    private Story _currentStory;
+    public bool dialogueIsPlaying { get; private set; }
+
+    private static DialogueManager instance;
+
+    [SerializeField] float _typingSpeed;
+
+    private Coroutine displayLineCoroutine;
+
+    private bool canContinue;
+
+    private void Awake()
     {
-        
+        if(instance != null)
+        {
+            Debug.Log("Found more than one dialogue manager in the scence");
+        }
+
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static DialogueManager GetInstance()
+    {
+        return instance;
+    }
+
+    private void Update()
     {
         
     }
