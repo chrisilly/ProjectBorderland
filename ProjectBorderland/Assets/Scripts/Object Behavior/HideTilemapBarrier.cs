d05db520dcc392ecbb49b0e97d282f085e962fa8
Allan-Majid <112650724+Allan-Majid@users.noreply.github.com>
2023-05-07 15:47:29 +0200

    Added a script for the developers to easily toggle the visibility of a tilemap barrier
    Apply the script to a tilemap and press "K" to toggle tilemap visibility on/off

---
+++ ProjectBorderland/Assets/Scripts/Object Behavior/HideTilemapBarrier.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HideTilemapBarrier : MonoBehaviour
+{
+    bool tilemapIsHidden;
+    // Start is called before the first frame update
+    void Awake()
+    {
+       this.gameObject.GetComponent<TilemapRenderer>().enabled = false; 
+        tilemapIsHidden = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tilemapIsHidden)
+        {
+            if(Input.GetKeyDown(KeyCode.K))
+            {
+                this.gameObject.GetComponent<TilemapRenderer>().enabled = true;
+                tilemapIsHidden=false;
+            }
+        }
+        else if (!tilemapIsHidden) 
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                this.gameObject.GetComponent<TilemapRenderer>().enabled = false;
+                tilemapIsHidden = true;
+            }
+        }
+    }
+}
