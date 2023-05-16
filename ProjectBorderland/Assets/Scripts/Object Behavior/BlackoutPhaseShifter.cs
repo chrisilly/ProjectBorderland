e0fa8522451d7afae18f2607ac89e879aa4535ae
Allan-Majid <112650724+Allan-Majid@users.noreply.github.com>
2023-05-16 03:02:31 +0200

    blackout section finished

--- ProjectBorderland/Assets/Scripts/Object Behavior/BlackoutPhaseShifter.cs
+++ ProjectBorderland/Assets/Scripts/Object Behavior/BlackoutPhaseShifter.cs
@@ -15,8 +15,9 @@ public class BlackoutPhaseShifter : MonoBehaviour
     [SerializeField] List<GameObject> crystalList = new List<GameObject>();
     [SerializeField] Tilemap tilemap;
     [SerializeField] GameObject nextCrystal;
+    [SerializeField] GameObject secondaryNextCrystal;
 
-    bool isCrystalActive = true;
+    //bool isCrystalActive = true;
 
     [SerializeField] float inactiveCrystalTimerLimit = 2.5f;
     float inactiveCrystalTimer = 0;
@@ -27,6 +28,16 @@ public class BlackoutPhaseShifter : MonoBehaviour
 
     private void Awake()
     {
+        GameObject[] blackoutCrystals = GameObject.FindGameObjectsWithTag("Blackout Crystal");
+        foreach (GameObject crystal in blackoutCrystals)
+        {
+            crystalList.Add(crystal);
+        }
+        GameObject[] blackoutTilemaps = GameObject.FindGameObjectsWithTag("Blackout Tilemap");
+        foreach (GameObject tilemap in blackoutTilemaps)
+        {
+            tilemapList.Add(tilemap);
+        }
         HideAllPlatformsAndCrystalsInLists();
         ActivateThePrimaryCrystalInBlackoutSection();
 
@@ -39,7 +50,7 @@ public class BlackoutPhaseShifter : MonoBehaviour
         if (collision.tag == "Player")
         {
             HideAllPlatformsAndCrystalsInLists();
-            isCrystalActive = false; //Activates update method
+            //isCrystalActive = false; //Activates update method
 
             //Phase Indicator Color Switching
             GameObject.Find("Phase Indicator").GetComponent<Image>().color = this.gameObject.GetComponent<SpriteRenderer>().color;
@@ -47,27 +58,33 @@ public class BlackoutPhaseShifter : MonoBehaviour
             tilemap.GetComponent<TilemapCollider2D>().enabled = true; //Enables collision with the platform that is linked with the crystal. E.G. Red crystal is linked with the red platforms.
             nextCrystal.GetComponent<SpriteRenderer>().enabled = true;
             nextCrystal.GetComponent<BoxCollider2D>().enabled = true;
+            if (this.gameObject.name == "Red Phase Crystal (6)")
+            {
+                GameObject.Find("Green Phase Crystal (24)").GetComponent<SpriteRenderer>().enabled = true;
+                GameObject.Find("Green Phase Crystal (24)").GetComponent<BoxCollider2D>().enabled = true;
+            }
 
             SetAlpha(1, tilemap);//Changes opacity of linked platform to full.
             this.gameObject.GetComponent<SpriteRenderer>().enabled = false; //Hides crystal
             this.gameObject.GetComponent<BoxCollider2D>().enabled = false; //Disables crystal collision
         }
+
     }
 
     private void Update() //Crystal reappearing again after player has collided with it.
     {
-        if (isCrystalActive == false)
-        {
-            inactiveCrystalTimer += Time.deltaTime;
-            if (inactiveCrystalTimer >= inactiveCrystalTimerLimit)
-            {
-                isCrystalActive = true;
-                this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
-                this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
-                inactiveCrystalTimer = 0;
+        //if (isCrystalActive == false)
+        //{
+        //    inactiveCrystalTimer += Time.deltaTime;
+        //    if (inactiveCrystalTimer >= inactiveCrystalTimerLimit)
+        //    {
+        //        isCrystalActive = true;
+        //        this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        //        this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        //        inactiveCrystalTimer = 0;
 
-            }
-        }
+        //    }
+        //}
 
         if (Input.GetButtonDown("Respawn")) //Phase Indicator color switching
         {
@@ -99,7 +116,7 @@ public class BlackoutPhaseShifter : MonoBehaviour
         }
     }
 
-    public void SetAlpha(float alpha,Tilemap _tilemap) //Set opacity method
+    public void SetAlpha(float alpha, Tilemap _tilemap) //Set opacity method
     {
 
         Color colorController = _tilemap.color;
