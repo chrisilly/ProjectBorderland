b47fd2fc8d250df95e847a1b7d7137a84d560f0d
Allan-Majid <112650724+Allan-Majid@users.noreply.github.com>
2023-05-19 18:02:02 +0200

    transition now works and menu uses coroutines now

--- ProjectBorderland/Assets/Scripts/Menu, Settings and Scene Manager/MainMenu.cs
+++ ProjectBorderland/Assets/Scripts/Menu, Settings and Scene Manager/MainMenu.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] AudioSource clickSFX;
-    public void PlayGame()
+    [SerializeField] float sfxDurationTime = 1f;
+    public IEnumerator PlayGame()
     {
+        PlayClickSFX();
+        yield return new WaitForSeconds(sfxDurationTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame()
+    public IEnumerator QuitGame()
     {
+        PlayClickSFX();
+        yield return new WaitForSeconds(sfxDurationTime);
         Debug.Log("You Quit");
         Application.Quit();
     }
@@ -20,5 +26,15 @@ public class MainMenu : MonoBehaviour
     public void PlayClickSFX()
     {
         clickSFX.Play();
+    }
+
+    public void PlayRoutineWrap()
+    {
+        StartCoroutine(PlayGame());
+    }
+
+    public void QuitRoutineWrap()
+    {
+        StartCoroutine (QuitGame());
     }
 }
