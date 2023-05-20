6c902cecc60894ef9e9dc57044bfe0fa84a6842e
Allan-Majid <112650724+Allan-Majid@users.noreply.github.com>
2023-05-20 13:31:29 +0200

    created a setting script, enabled slider handles

---
+++ ProjectBorderland/Assets/Scripts/Menu, Settings and Scene Manager/SettingsMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void SetSFXVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+    
+    public void SetMusicVolume(float volume)
+    {
+
+    }
+}
