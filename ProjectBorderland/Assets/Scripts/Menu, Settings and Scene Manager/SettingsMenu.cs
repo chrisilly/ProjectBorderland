78d89e4a793a3d9d292f74f13d9eb28cdcaaa9c6
Allan-Majid <112650724+Allan-Majid@users.noreply.github.com>
2023-05-20 14:24:30 +0200

    settings menu is now functional except for volume controls

--- ProjectBorderland/Assets/Scripts/Menu, Settings and Scene Manager/SettingsMenu.cs
+++ ProjectBorderland/Assets/Scripts/Menu, Settings and Scene Manager/SettingsMenu.cs
@@ -1,16 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
+
+    Resolution[] resolutions;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width==Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) 
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
     public void SetSFXVolume(float volume)
     {
         Debug.Log(volume);
     }
-    
+
     public void SetMusicVolume(float volume)
     {
 
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
